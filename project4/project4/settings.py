ca05f175cfd61803f5f7a31c249911ed64a5ee98
kumbaine <kumbainejesse@gmail.com>
2024-05-25 02:15:37 +0300

    changed allowed hosts

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("debug")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("allowed_hosts")
              
 
 # Application definition
